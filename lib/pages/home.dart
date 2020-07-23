8ddeb7052a4bb8e6a9cd9f8380a522d6c1cbdc34
Chinaza <ogwo.chinaza@yahoo.com>
2020-07-23 14:21:55 +0100

    Initial commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'fly.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hero Animation'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: InkWell(
+            child: Hero(
+              tag: 'format_paint',
+              child: Icon(
+                Icons.format_paint,
+                color: Colors.lightGreen,
+                size: 120.0,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Fly()),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
